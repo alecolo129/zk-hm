cbed233173810958535d8774da0066322108b859
Alessandro Colombo <alessandro.colombo@ait.ac.at>
2026-05-17 19:22:13 +0200

    Fix build with asan

--- build.sh
+++ build.sh
@@ -10,19 +10,26 @@ helpFunction()
   echo ""
   echo "Usage: $0 -c -h"
   echo -e "\t-c If present, starts the building phase from a clean state."
+  echo -e "\t-s If present, instrument build with address sanitizer."
   echo -e "\t-h Print the script help message."
   exit 1
 }
 
-while getopts "ch" opt
+while getopts "chs" opt
 do
    case "$opt" in
       c )
         rm -rf build/* ;;
+      s )
+        sanitize=1     ;;
       h ) helpFunction ;;
       ? ) helpFunction ;;
    esac
 done
 
-cmake -S . -B build -G Ninja
+if [[ -v sanitize ]]; then
+  cmake -S . -B build -G Ninja -DENABLE_SANITIZERS=ON
+else
+  cmake -S . -B build -G Ninja
+fi
 cmake --build build
